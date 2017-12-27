2a14b5b7bf6cd76074f47bff1c201c3912776292
Otávio Gaião <otaviogaiao@gmail.com>
2017-12-27 12:32:58 -0400

    Corrige problema de opacidade do componente Botão

--- components/Button.js
+++ components/Button.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import PropTypes from 'prop-types'
 
-//TODO: arrumar o problema com a opacidade
 export default function Button({text, onPress, style, disabled, textStyle}) {
-    let opacity = disabled === true ? 0.5 : 1
-    // console.log('opacity', opacity)
+    const opacity = disabled ? 0.5 : 1
+
     return (
-        <TouchableOpacity onPress={onPress} style={[defaultStyles.button, style, {opacity: opacity}]} 
+        <TouchableOpacity onPress={onPress}
             disabled={disabled}>
-            <Text style={[defaultStyles.text, textStyle]}>{text}</Text>
+            <View style={[defaultStyles.button, style, {opacity}]}>
+                <Text style={[defaultStyles.text, textStyle]}>{text}</Text>
+            </View>
         </TouchableOpacity>
     )
 
