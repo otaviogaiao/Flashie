0c5c52775691648f7a0c91efc5e8c0dc890550ae
Otávio Gaião <otaviogaiao@gmail.com>
2017-12-27 12:36:39 -0400

    comentários adicionados

--- App.js
+++ App.js
@@ -10,6 +10,14 @@ import reducer from './reducers'
 
 const store = createStore(reducer, applyMiddleware(thunkMiddeware))
 
+/*
+  Features extras:
+    Editar e deletar um card já criado.(utilizando swipe left)
+    Exibir a maior pontuação ao final de um quiz.
+    Desabilitar as notificações atráves de tela própria dentro do app.
+    Deletar um deck (utilizando swipe right)
+*/
+
 export default class App extends React.Component {
   render() {
     return (
